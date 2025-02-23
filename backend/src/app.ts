251503f53fa8a6540a698a1642ced87042412228
RichardRain <alexander.hell.scorpion@gmail.com>
2025-02-23 10:02:43 +0300

    feat: Реализован CORS, обновлен конфиг бэкенда

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,13 +5,12 @@ import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
-import { DB_ADDRESS } from './config'
+import { DB_ADDRESS, PORT, ORIGIN_ALLOW } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 import rateLimit from 'express-rate-limit'
 
-const { PORT = 3000 } = process.env
 const app = express()
 
 app.use(rateLimit({
@@ -24,9 +23,7 @@ app.use(rateLimit({
 
 app.use(cookieParser())
 
-app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 
